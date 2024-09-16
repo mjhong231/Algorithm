f1943f5ec4211f1ed0d6229e19db60d250be063a
mjhong231 <biotic0523@naver.com>
2024-09-16 20:47:45 +0900

    [Silver III] Title: N과 M (7), Time: 1796 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -2,7 +2,6 @@ N, M = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
 
 path = []
-visited = [0] * (N+1)
 
 def program(level):
     if level == M:
@@ -10,10 +9,9 @@ def program(level):
         return
 
     for i in range(N):
-        if visited[i] == 1: continue
         path.append(arr[i])
         program(level+1)
         path.pop()
-        visited[i] = 0
+
 
 program(0)
