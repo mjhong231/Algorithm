7dcff5f90a6c7803467bc58cac09bbb7a349068b
mjhong231 <biotic0523@naver.com>
2024-10-03 18:14:58 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3504 ms, Memory: 190968 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 
 def bfs(x, y, z):  
     q = deque()
-    q.append((x,y,z))
+    q.append((x,y,z))  
 
     while q:
         x, y, z = q.popleft()
@@ -16,11 +16,11 @@ def bfs(x, y, z):
         for dx, dy in dir:
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < M:  
-                if MAP[nx][ny] == 1 and z == 0 and visit[nx][ny][1] == 0:
+                if MAP[nx][ny] == 1 and z == 0:
                     visit[nx][ny][1] = visit[x][y][z] + 1  
-                    q.append((nx, ny, 1))
-                elif MAP[nx][ny] == 0 and visit[nx][ny][z] == 0:  
-                    visit[nx][ny][z] = visit[x][y][z] + 1 
+                    q.append((nx, ny, 1))  
+                elif MAP[nx][ny] == 0 and visit[nx][ny][z] == 0: 
+                    visit[nx][ny][z] = visit[x][y][z] + 1  
                     q.append((nx, ny, z))
     return -1
 
@@ -32,7 +32,7 @@ N, M = map(int, input().split())
 
 MAP = [list(map(int, input().strip())) for _ in range(N)]
 
-visit = [[[0] * 2 for _ in range(M)] for _ in range(N)]  
+visit = [[[0] * 2 for _ in range(M)] for _ in range(N)] 
 
 visit[0][0][0] = 1   
 
