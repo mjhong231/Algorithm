eb65dad23f47ce80634cab16bbaf54324a684caa
mjhong231 <biotic0523@naver.com>
2024-10-13 15:45:38 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7360 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = [0] * 10001
+for _ in range(1, N+1):
+    arr[int(input())] += 1
+
+for i in range(len(arr)):
+    if arr[i] != 0:
+        for _ in range(arr[i]):
+            print(i)
