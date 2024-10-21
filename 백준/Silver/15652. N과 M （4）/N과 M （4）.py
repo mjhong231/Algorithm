02f9b2109123b0280d138df8cff2f8c2fb41d659
mjhong231 <biotic0523@naver.com>
2024-10-22 01:44:55 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,8 +1,6 @@
-
 N, M = map(int, input().split())
 
 path = []
-visited = [0] * (N+1)
 
 def dfs(start, level):
     if level == M:
@@ -10,11 +8,9 @@ def dfs(start, level):
         return
 
     for i in range(start, N+1):
-
-        visited[i] = 1
         path.append(i)
         dfs(i, level+1)
         path.pop()
-        visited[i] = 0
+
 
 dfs(1, 0)
