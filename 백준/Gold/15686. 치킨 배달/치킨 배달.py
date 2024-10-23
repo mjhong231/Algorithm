b1776102b148e2d848c0ab4e5d5e63da6c178d2a
mjhong231 <biotic0523@naver.com>
2024-10-23 14:56:08 +0900

    [Gold V] Title: 치킨 배달, Time: 340 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,4 +1,3 @@
-from itertools import combinations
 import sys
 input = sys.stdin.readline
 
@@ -8,24 +7,31 @@ N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 result = float('inf')
-houses = []  # 집
-chickens = [] # 치킨집
+houses = []  
+chickens = [] 
 
 for i in range(N):
     for j in range(N):
         if arr[i][j] == 2:
-            chickens.append((i, j))
+            chickens.append((i, j)) 
         elif arr[i][j] == 1:
-            houses.append((i, j))
+            houses.append((i, j))   
 
-for chicken in combinations(chickens, M):
-    temp = 0  # 치킨거리 초기화
-    for house in houses:
-        chicken_len = float('inf')
-        for j in range(M):
-            chicken_len = min(chicken_len, abs(house[0] - chicken[j][0]) + abs(house[1] - chicken[j][1]))
-        temp += chicken_len
-    result = min(result, temp)
+def dfs(start, path):
+    global result
+    if len(path) == M:  
+        temp = 0  
+        for house in houses: 
+            chicken_len = float('inf')  
+            for chicken in path: 
+                chicken_len = min(chicken_len,  abs(house[0] - chicken[0]) + abs(house[1] - chicken[1]))
+            temp += chicken_len 
+        result = min(result, temp)   
+        return  
 
+    for i in range(start, len(chickens)):  
+        dfs(i+1, path + [chickens[i]])     
+
+dfs(0, [])  
 
 print(result)
