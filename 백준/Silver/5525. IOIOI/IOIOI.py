3aef93c38b7d846228982d0fac6a8760da1e683f
mjhong231 <biotic0523@naver.com>
2024-12-16 11:21:15 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,15 +1,24 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline().strip())
-M = int(sys.stdin.readline().strip())
-S = sys.stdin.readline().strip()
+N = int(input())   # IOI 패턴의 반복 횟수
+M = int(input())   # 문자열 S의 길이
+S = input().strip()  # 입력 문자열
 
-P = 'IO'*N + 'I'
-
+now = 0  
 cnt = 0
-for i in range(M):
-  if S[i] == 'I':
-    if S[i:i+(N*2+1)] == P and i <= M:
-      cnt += 1
+result = 0  
 
-print(cnt)
+while now < M - 1:  
+    if S[now:now + 3] == 'IOI':
+        cnt += 1
+        now += 2
+        if cnt == N:
+            result += 1
+            cnt -= 1
+    
+    else:
+        cnt = 0
+        now += 1
+        
+print(result)
