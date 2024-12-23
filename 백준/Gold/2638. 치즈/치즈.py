7baa9b8556cb3b9ef0e0311c4290295c2f0951c6
mjhong231 <biotic0523@naver.com>
2024-12-23 13:50:53 +0900

    [Gold III] Title: 치즈, Time: 200 ms, Memory: 116312 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,58 +1,50 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
+
 from collections import deque
 
-#공기 탐색
-def air_bfs(i,j):
-    q=deque()
-    q.append([i,j])
-    visited[i][j]=1
+def bfs(): # 외부 공기와 접촉한 치즈 찾기
+    visited = [[False] * M for _ in range(N)]
+    q = deque()
+    q.append((0, 0))
+    visited[0][0] = True
 
     while q:
-        x,y=q.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<n and 0<=ny<m:
-            	#다음 진행이 공기면(큐 추가, 방문처리)
-                if visited[nx][ny]==0 and real_graph[nx][ny]==0:
-                    q.append([nx,ny])
-                    visited[nx][ny]=1
-                #다음 진행이 치즈면(방문정보 업데이트)
-                elif real_graph[nx][ny]==1:
-                    visited[nx][ny]=visited[nx][ny]+1
+        y, x = q.popleft()
+        for dy, dx in dirs:
+            ny = y + dy
+            nx = x + dx
 
-n, m=map(int, input().split())
-real_graph=[]
-for i in range(n):
-    real_graph.append(list(map(int, input().split())))
+            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+                if MAP[ny][nx] >= 1: # 치즈가 있으면
+                    MAP[ny][nx] += 1 # 접촉 체크
+                else:
+                    q.append((ny, nx)) # 치즈가 없으면 다음 탐색
+                    visited[ny][nx] = True
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-time=0
+def f(): # 치즈 녹이기
+    melted = 0
+    for i in range(N):
+        for j in range(M):
+            if MAP[i][j] >= 3: # 2번 이상 공기와 접촉했으면 녹이기
+                MAP[i][j] = 0
+                melted += 1
+            elif MAP[i][j] >= 2: # 한번만 접촉한 치즈는 초기화
+                MAP[i][j] = 1
+    return melted
 
-while 1:
-    visited=[[0 for _ in range(m)] for _ in range(n)]
+N, M = map(int, input().split())  # N 세로격자(열) / M 가로격자 (행)
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+cycle = 0
+MAP = [list(map(int, input().split())) for _ in range(N)]
 
-    air_bfs(0,0)
-    #탐색 한바퀴 끝나면 시간+1
-    time+=1
-	
-    #두면이상 공기랑 닿으면, 빈칸 처리
-    for i in range(n):
-        for j in range(m):
-            if visited[i][j]>=2:
-                real_graph[i][j]=0
+time = 0
 
-    # 공기 카운트
-    block_cnt=0
-    for i in range(n):
-        for j in range(m):
-            if real_graph[i][j]==0:
-                block_cnt+=1
-    #탐색 한번 하고 난 그래프의 공기수가 배열의 크기랑 같으면 break
-    if block_cnt==(n*m):
+while True:
+    bfs()
+    melted = f()
+    if melted:
+        time += 1
+    else:
+        print(time)
         break
-    
-
-print(time)
